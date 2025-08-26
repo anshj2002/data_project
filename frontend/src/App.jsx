ef7bf74f2c1399fdbf98da2ce1f8350bf0e654b0
omaxe <Colocal.dubai@gmail.com>
2025-08-27 01:17:25 +0530

    added logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import WeatherForm from "./components/WeatherForm";
 import WeatherList from "./components/WeatherList";
 
